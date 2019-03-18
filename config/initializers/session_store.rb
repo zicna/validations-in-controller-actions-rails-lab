5ad0449b4f4312bc6ffcd20c3aa7c6ed9ca0862c
maxwellbenton <maxwellbenton@gmail.com>
2019-03-18 11:18:17 -0400

    rails app:update to 5.0.7.1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_validations-in-controller-actions-rails-lab_session'
+Rails.application.config.session_store :cookie_store, key: '_validations_in_controller_actions_rails_lab_session'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/authors/new", to: "authors#new", as: "new_author"
   get "/authors/:id", to: "authors#show", as: "author"
   post "/authors", to: "authors#create"
