ed8b4b09b3339bce53baf4c28067814df410592d
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-04-27 19:21:39 -0500

    commit tree 4bb20b8eb4271660c5dc1a5c86e4d9acae60a69b

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,12 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,13 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+
+    
   end
 
   private
